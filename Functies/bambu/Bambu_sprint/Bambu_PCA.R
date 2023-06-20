91cb26e81a0242de427f908b237604d490cf8c66
wasung <131241105+wasung@users.noreply.github.com>
2023-06-20 23:56:46 +0200

    Update Bambu_PCA.R
    added heatmap to the script

--- Functies/bambu/Bambu_sprint/Bambu_PCA.R
+++ Functies/bambu/Bambu_sprint/Bambu_PCA.R
@@ -1,20 +1,24 @@
+# Load required packages
 library(bambu)
 library(ggplot2)
 library(GenomicFeatures)
+library(BiocParallel)
+library(circlize)
+library(ComplexHeatmap)
+
+# Set the number of threads for parallel processing
+register(SnowParam(8))
 
 # Set file paths
-bam.folder <- "/home/data/projecticum/PFAS/BAM_files"
-gff.file <- "~/GRCh38_latest_genomic.gff"
-genome.file <- "~/GRCh38_latest_rna.fna"
+bam.folder <- "~/Project_data/Filtered_BAM"
+annotation.file <- "~/Project_data/Filtered_BAM/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gtf."
+genome.file <- "~/Project_data/Filtered_BAM/GRCh38_latest_rna.fa"
 
 # Get all .bam files in the BAM_filtered directory
 bam.files <- list.files(bam.folder, pattern = "\\.bam$", full.names = TRUE)
 
-# Import GFF file as a GRanges object
-annotations <- import.gff(con = gff.file)
-
 # Prepare annotations
-prepared.annotations <- prepareAnnotations(annotations)
+prepared.annotations <- prepareAnnotations(annotation.file)
 
 # Run bambu to get summarized experiment object
 se.multiSample <- bambu(reads = bam.files, annotations = prepared.annotations, genome = genome.file)
@@ -24,3 +28,14 @@ writeBambuOutput(se.multiSample, path = "~/project_genexpressie/bambu/")
 
 # Plot the samples in PCA
 plotBambu(se.multiSample, type = "pca")
+
+# Obtain gene expression estimates from transcript expression
+colData(se.multiSample)$condition <- as.factor(basename(bam.files))
+
+seGene.multiSample <- transcriptToGeneExpression(se.multiSample)
+
+# Set group variable for plotting
+colData(seGene.multiSample)$groupVar <- as.factor(basename(bam.files))
+
+# Plot gene expression heatmap
+plotBambu(seGene.multiSample, type = "heatmap")
