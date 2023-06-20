b8c0f27818ffc625087c6acbed2af3892b29ec07
nonnehodes01 <Nonne.hodes@gmail.com>
2023-06-20 20:46:12 +0200

    heatmap functie

---
+++ Functies/bambu/bambu_functie_heatmap.R
@@ -0,0 +1,28 @@
+library(bambu)
+library(GenomicFeatures)
+
+run_bambu_pipeline <- function(bam_folder, annotation_file, genome_file, output_folder) {
+  # Get all BAM files
+  bam_files <- list.files(bam_folder, pattern = "\\.bam$", full.names = TRUE)
+  
+  # Prepare annotations
+  prepared_annotations <- prepareAnnotations(annotation_file)
+  
+  # Get summarised experiment object
+  se_multisample <- bambu(reads = bam_files, annotations = prepared_annotations, genome = genome_file)
+  
+  # Write Bambu output
+  writeBambuOutput(se_multisample, path = output_folder)
+  
+  # Plot Bambu heatmap
+  plotBambu(se_multisample, type = "heatmap")
+}
+
+# Set file paths
+bam_folder <- "/home/data/projecticum/PFAS/BAM_files"
+annotation_file <- "~/project_genexpressie/annotation/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gtf.gz"
+genome_file <- "/home/data/projecticum/PFAS/raw_data/GRCh38_latest_rna.fna.gz"
+output_folder <- "~/project_genexpressie/bambu"
+
+# Run the Bambu pipeline
+run_bambu_pipeline(bam_folder, annotation_file, genome_file, output_folder)
