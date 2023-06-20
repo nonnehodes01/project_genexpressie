ab06a8708dfb844ab4d262d0600a686f74e76fa4
nonnehodes01 <Nonne.hodes@gmail.com>
2023-06-20 22:00:44 +0200

    bambu

--- Functies/bambu/bambu_functie_heatmap.R
+++ Functies/bambu/bambu_functie_heatmap.R
@@ -19,10 +19,10 @@ run_bambu_pipeline <- function(bam_folder, annotation_file, genome_file, output_
 }
 
 # Set file paths
-bam_folder <- "/home/data/projecticum/PFAS/BAM_files"
-annotation_file <- "~/project_genexpressie/annotation/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gtf.gz"
-genome_file <- "/home/data/projecticum/PFAS/raw_data/GRCh38_latest_rna.fna.gz"
-output_folder <- "~/project_genexpressie/bambu"
+#bam_folder <- "/home/data/projecticum/PFAS/BAM_files"
+#annotation_file <- "~/project_genexpressie/annotation/GCA_000001405.15_GRCh38_full_analysis_set.refseq_annotation.gtf.gz"
+#genome_file <- "/home/data/projecticum/PFAS/raw_data/GRCh38_latest_rna.fna.gz"
+#output_folder <- "~/project_genexpressie/bambu"
 
 # Run the Bambu pipeline
 run_bambu_pipeline(bam_folder, annotation_file, genome_file, output_folder)
